a19643517a1e0769e61284fd39f4be2fe1421e95
Michael Townsend <michael@doublespeakgames.com>
2013-10-31 14:37:19 -0400

    Update prestige.js
    Null check in Prestige.load

--- script/prestige.js
+++ script/prestige.js
@@ -59,30 +59,32 @@ var Prestige = {
   
 	  load : function() {
 		var prevStores = $SM.get('previous.stores');
-		$SM.add('stores["wood"]', prevStores[0]);
-		$SM.add('stores["fur"]', prevStores[1]);
-		$SM.add('stores["meat"]', prevStores[2]);
-		$SM.add('stores["iron"]', prevStores[3]);
-		$SM.add('stores["coal"]', prevStores[4]);
-		$SM.add('stores["sulphur"]', prevStores[5]);
-		$SM.add('stores["steel"]', prevStores[6]);
-		$SM.add('stores["cured meat"]', prevStores[7]);
-		$SM.add('stores["scales"]', prevStores[8]);
-		$SM.add('stores["teeth"]', prevStores[9]);
-		$SM.add('stores["leather"]', prevStores[10]);
-		$SM.add('stores["bait"]', prevStores[11]);
-		$SM.add('stores["torch"]', prevStores[12]);
-		$SM.add('stores["cloth"]', prevStores[13]);
-		$SM.add('stores["bone spear"]', prevStores[14]);
-		$SM.add('stores["iron sword"]', prevStores[15]);
-		$SM.add('stores["steel sword"]', prevStores[16]);
-		$SM.add('stores["bayonet"]', prevStores[17]);
-		$SM.add('stores["rifle"]', prevStores[18]);
-		$SM.add('stores["laser rifle"]', prevStores[19]);
-		$SM.add('stores["bullets"]', prevStores[20]);
-		$SM.add('stores["energy cell"]', prevStores[21]);
-		$SM.add('stores["grenade"]', prevStores[22]);
-		$SM.add('stores["bolas"]', prevStores[23]);
+		if(prevStores != null) {
+			$SM.add('stores["wood"]', prevStores[0]);
+			$SM.add('stores["fur"]', prevStores[1]);
+			$SM.add('stores["meat"]', prevStores[2]);
+			$SM.add('stores["iron"]', prevStores[3]);
+			$SM.add('stores["coal"]', prevStores[4]);
+			$SM.add('stores["sulphur"]', prevStores[5]);
+			$SM.add('stores["steel"]', prevStores[6]);
+			$SM.add('stores["cured meat"]', prevStores[7]);
+			$SM.add('stores["scales"]', prevStores[8]);
+			$SM.add('stores["teeth"]', prevStores[9]);
+			$SM.add('stores["leather"]', prevStores[10]);
+			$SM.add('stores["bait"]', prevStores[11]);
+			$SM.add('stores["torch"]', prevStores[12]);
+			$SM.add('stores["cloth"]', prevStores[13]);
+			$SM.add('stores["bone spear"]', prevStores[14]);
+			$SM.add('stores["iron sword"]', prevStores[15]);
+			$SM.add('stores["steel sword"]', prevStores[16]);
+			$SM.add('stores["bayonet"]', prevStores[17]);
+			$SM.add('stores["rifle"]', prevStores[18]);
+			$SM.add('stores["laser rifle"]', prevStores[19]);
+			$SM.add('stores["bullets"]', prevStores[20]);
+			$SM.add('stores["energy cell"]', prevStores[21]);
+			$SM.add('stores["grenade"]', prevStores[22]);
+			$SM.add('stores["bolas"]', prevStores[23]);
+		}
 		return prevStores;
 	},
 
