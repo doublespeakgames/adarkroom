cfaeb516031380897510375e82eadea13e7d713f
vanadar <vanadar.univers@gmail.com>
2014-02-08 16:02:08 +0100

    corrections sur des traductions

--- script/localization.js
+++ script/localization.js
@@ -6,9 +6,10 @@
 	                 _("charcutier"),_('iron miner'),_('coal miner'), _('sulphur miner'),  _('armourer'),
 	                 _('steelworker'),_('bait'),_('cured meat'), _('scales'), _('compass'),
 	                 _('gatherer'),_('cloth'), _('scales'), _('cured meat'), _('thieves'),
-	                 _('not enough fur.'), _('not enough wood.'), _('not enough coal.'), _('not enough iron.'), _('not enough steel.'), _('baited trap'),
-			_('not enough scales.'),  _('not enough teeth.'), _('not enough leather.'),
-	                 _('the compass points east.'), _('the compass points west.'), _('the compass points north.'), _('the compass points south.')]; 
+	                 _('not enough fur'), _('not enough wood'), _('not enough coal'), _('not enough iron'), _('not enough steel'), _('baited trap'),
+			_('not enough scales'),  _('not enough teeth'), _('not enough leather'),
+	                 _('the compass points east.'), _('the compass points west.'), _('the compass points north.'), _('the compass points south.'), 
+	                 _('the compass points northeast.'), _('the compass points northwest.'), _('the compass points southeast.'), _('the compass points southwest.')]; 
 	delete keywords;
 	
 	//translate text in css by overriding attributes
