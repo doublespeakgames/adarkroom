a0aa2eaab932ea202cff4e14e39a9d12d8e737d3
Blake Grotewold <blake.grotewold@gmail.com>
2014-04-01 19:25:59 -0500

    Add disableSelection to stop highlighting

--- script/engine.js
+++ script/engine.js
@@ -85,6 +85,8 @@ var Engine = {
 		if(Engine.isMobile()) {
 			window.location = 'mobileWarning.html';
 		}
+
+		Engine.disableSelection();
 		
 		if(this.options.state != null) {
 			window.State = this.options.state;
@@ -564,6 +566,11 @@ var Engine = {
 		if(Engine.activeModule.swipeDown) {
 			Engine.activeModule.swipeDown(e);
 		}
+	},
+
+	disableSelection: function() {
+		document.onselectstart = function() {return false;} // this is for IE
+		document.onmousedown = function() {return false;} // this is for the rest
 	},
 	
 	handleStateUpdates: function(e){
