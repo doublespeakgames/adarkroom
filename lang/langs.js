3995201c08c4add4b5afea56843ac78b14b12c64
AndySky21 <master.skywalker.88@gmail.com>
2014-11-10 23:17:52 +0100

    Update langs.js
    inserted it - italiano

--- lang/langs.js
+++ lang/langs.js
@@ -2,12 +2,13 @@ var langs = {
 	'cn':'简体中文',
 	'de':'deutsch',
 	'en':'english',
-	'es': 'español',
+	'es':'español',
 	'fr':'français',
 	'kr':'한국어',
 	'pl':'polski',
 	'ru':'русский',
 	'sv':'svenska',
 	'tr':'türkçe',
-	'uk':'українська'
+	'uk':'українська',
+	'it':'italiano'
 };
