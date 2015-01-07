621c14c2ae44ffd4fccc72102b061572a644ab4f
chuglydude <lk@Ethan-MacBook-Air.local>
2015-01-06 22:03:07 -0800

    Fixed Touch Joy Stick
    Put it in engine.js file

--- script/engine.js
+++ script/engine.js
@@ -684,3 +684,16 @@ $.Dispatch = function( id ) {
 $(function() {
 	Engine.init();
 });
+ 	node.addEventListener("touchmove", function(event){
+    //only deal with one finger
+    if(event.touches.length == 1){
+    //get the information for finger #1
+    var touch = event.touches[0],
+    // find style object for the node the drag started from
+    style = touch.target.style;
+   // position the element under the touch point
+   style.position = "absolute";
+   style.left = touch.pageX + "px";
+   style.top = touch.pageY + "px";
+    }
+}, false); 
