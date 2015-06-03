6830b106d4d9707dc2b34678c53d4d563d2eea35
Andrea Rendine <AndySky21@users.noreply.github.com>
2015-06-03 14:46:56 +0200

    Always find the ship
    Fixed possibility not to find W

--- script/world.js
+++ script/world.js
@@ -159,15 +159,11 @@ var World = {
 		//subscribe to stateUpdates
 		$.Dispatch('stateUpdate').subscribe(World.handleStateUpdates);
 		
-		// the ship has not been found yet. Map it and show compass tooltip
+		// Map the ship and show compass tooltip
 		World.ship = World.mapSearch(World.TILE.SHIP,$SM.get('game.world.map'),1);
-		if(World.ship.length > 0){
-			World.dir = World.compassDir(World.ship[0]);
-			// compass tooltip text
-			Room.compassTooltip(World.dir);
-		} else {
-			World.dir = null;
-		}
+		World.dir = World.compassDir(World.ship[0]);
+		// compass tooltip text
+		Room.compassTooltip(World.dir);
 	},
 	
 	clearDungeon: function() {
