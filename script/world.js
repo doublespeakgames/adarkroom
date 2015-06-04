0fe099ac7061f9ca4e126272b270d2bc10b93169
Andrea Rendine <AndySky21@users.noreply.github.com>
2015-06-04 17:17:56 +0200

    key restore on coming home
    Returning home sets keyRestore to true. Keyboard navigation will be resumed when key is released.

--- script/world.js
+++ script/world.js
@@ -830,6 +830,7 @@ var World = {
 	
 	goHome: function() {
 		Engine.keyLock = true;
+		Engine.keyRestore = true;
 		// Home safe! Commit the changes.
 		$SM.setM('game.world', World.state);
 		if(World.state.sulphurmine && $SM.get('game.buildings["sulphur mine"]', true) === 0) {
