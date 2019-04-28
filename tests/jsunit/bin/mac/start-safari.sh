d0b852b86246811a27f3a836167d93640374d799
Ben Geyer <bgeyer1234@gmail.com>
2019-04-28 16:12:10 -0700

    Switched to jasmine

--- tests/jsunit/bin/mac/start-safari.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Starts Safari. Use this instead of calling the AppleScripts directly.
-
-osascript bin/mac/stop-safari.scpt
-osascript bin/mac/start-safari.scpt $1
-
--- tests/jsunit/bin/mac/stop-safari.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Stops Safari. Use this instead of calling the AppleScripts directly.
-
-osascript bin/mac/stop-safari.scpt
-
